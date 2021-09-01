853be16a789bb023478f8bba3db6b2011b63e7b3
MadhuriTeli <madhuriteli07@gmail.com>
2021-09-02 01:48:55 +0530

    contest flyer  in home page

--- javascript/script.js
+++ javascript/script.js
@@ -4,16 +4,16 @@
 
 $(document).ready(function () {
 
-  // $(".close").click(function () {
-  //   $('.container').hide();
-  //   $('#mn').show();
+  $(".close").click(function () {
+    $('.container').hide();
+    $('#mn').show();
 
-  // });
+  });
 
-  // $(".ind-img").click(function () {
-  //   $('.container').hide();
-  //   $('#mn').show();
+  $(".ind-img").click(function () {
+    $('.container').hide();
+    $('#mn').show();
 
-  // });
+  });
 
 });
